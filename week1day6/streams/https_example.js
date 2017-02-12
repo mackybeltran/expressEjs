2b48d1e5b3abb7c834f5bbd61d9b1b23d3c12e44
Macky Beltran <mackybeltran1@gmail.com>
2017-02-12 23:14:47 +0000

    adding responses

--- week1day6/streams/https_example.js
+++ week1day6/streams/https_example.js
@@ -1,6 +1,11 @@
 var https = require('https');
 
-console.log('I did it!');
+
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
 
 var options = {
   host: 'www.example.org',
@@ -8,8 +13,9 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
@@ -17,3 +23,4 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
